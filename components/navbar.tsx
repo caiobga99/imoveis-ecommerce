f3b8c8c447760e5bd7a85a746d271aa9874748ed
caiobga99 <caiobga99@gmail.com>
2024-09-26 03:21:52 -0300

    custom routing navbar.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -15,7 +15,7 @@ import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -26,31 +26,31 @@ import {
   HeartFilledIcon,
   SearchIcon,
   Logo,
-  UserIcon
+  UserIcon,
 } from "@/components/icons";
 
 export const Navbar = () => {
   const pathname = usePathname();
-  
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    <NextUINavbar maxWidth="2xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">IMOVEIS</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href} isActive={pathname === item.href} >
+            <NavbarItem key={item.href} isActive={pathname === item.href}>
               <NextLink
                 className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium ",
+                  linkStyles({
+                    color: pathname === item.href ? "primary" : "foreground",
+                  }),
+                  "data-[active=true]:text-primary data-[active=true]:font-large"
                 )}
-                color="foreground"
                 href={item.href}
               >
                 {item.label}
@@ -64,9 +64,8 @@ export const Navbar = () => {
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
-       <ThemeSwitch />
-        <NavbarItem className="hidden md:flex" >
-
+        <ThemeSwitch />
+        <NavbarItem className="hidden md:flex">
           <Button
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
@@ -77,9 +76,8 @@ export const Navbar = () => {
           >
             Entrar
           </Button>
-       
         </NavbarItem>
-        <NavbarItem className="hidden md:flex" >
+        <NavbarItem className="hidden md:flex">
           <Button
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
@@ -91,9 +89,7 @@ export const Navbar = () => {
             Cadastrar
           </Button>
         </NavbarItem>
-
       </NavbarContent>
-      
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <ThemeSwitch />
@@ -109,9 +105,8 @@ export const Navbar = () => {
                   item.href === pathname
                     ? "primary"
                     : item.href === "/logout"
-                      ? "danger"
-                      : "foreground"
-                      
+                    ? "danger"
+                    : "foreground"
                 }
                 href={item.href}
                 size="lg"
