f0eee5452a54c1560faae5d36ef6a1c9703b1b76
caiobga99 <caiobga99@gmail.com>
2024-09-27 03:49:57 -0300

    add Imovel interface.

---
+++ types/imovel.ts
@@ -0,0 +1,12 @@
+export interface Imovel {
+  id: string;
+  valor_aluguel: number;
+  valor_venda: number;
+  metros_quadrados: number;
+  qnt_camas: number;
+  qnt_carro: number;
+  endereco: string;
+  valor_condominio: number;
+  imagem: string;
+  tipo_imovel: string;
+}
