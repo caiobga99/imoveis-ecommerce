f526053b77f35d0146f94ce1f1026152a5fc2fb0
caiobga99 <caiobga99@gmail.com>
2024-09-28 08:28:50 -0300

    add error handling for user input fields in login and registration forms

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,75 +1,155 @@
 "use client";
-import { title } from "@/components/primitives";
-import { Checkbox, Input, Button } from "@nextui-org/react";
-import { EyeSlashFilledIcon, EyeFilledIcon } from "@/components/icons";
+import { Input, Button } from "@nextui-org/react";
 import { useState } from "react";
 import NextLink from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { EyeSlashFilledIcon, EyeFilledIcon } from "@/components/icons";
+
+// Definindo o esquema de validação com Zod
+const schema = z.object({
+  name: z.string().min(1, "Nome é obrigatório"),
+  email: z.string().email("Formato de email inválido"),
+  password: z.string().min(4, "A senha deve ter pelo menos 4 caracteres"),
+});
+
+// Tipo para os dados do formulário
+type FormData = z.infer<typeof schema>;
+
+const simulateApiCall = (data: FormData) => {
+  return new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      if (data.email === "teste@teste.com") {
+        resolve("Login bem-sucedido");
+      } else {
+        reject("Credenciais inválidas");
+      }
+    }, 1000);
+  });
+};
+
 export default function LoginPage() {
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-  const toggleVisibility = () => setIsVisible(!isVisible);
+  const toggleVisibility = () => setIsVisible((prev) => !prev);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setLoading(true);
+    setServerError(null);
+    setSuccessMessage(null);
+
+    try {
+      const message = await simulateApiCall(data);
+
+      setSuccessMessage(message);
+    } catch (error) {
+      setServerError(error as string);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <form className="w-full h-full flex flex-col gap-5 justify-between ">
-      <div className="flex flex-col text-left justify-center gap-2 ">
+    <form
+      className="w-full h-full flex flex-col gap-5 justify-between"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col text-left justify-center gap-2">
         <h1 className="text-2xl">Entre com sua conta</h1>
         <h2 className="text-base">Faça o login com sua conta para continuar</h2>
       </div>
-      <div className="flex flex-col justify-center h-full  w-full gap-4">
+      <div className="flex flex-col justify-center h-full w-full gap-4 text-left">
         <div>
           <Input
-            isClearable
-            type="name"
             label="Nome"
-            variant="underlined"
             placeholder="Digite seu nome"
-            onClear={() => console.log("input cleared")}
-            className="w-[full]"
+            variant="underlined"
+            {...register("name")}
+            isInvalid={!!errors.name}
             size="lg"
           />
+          {errors.name && (
+            <span className="text-red-500 text-sm mt-1 ml-2">
+              {errors.name.message}
+            </span>
+          )}
         </div>
         <div>
           <Input
-            isClearable
-            type="email"
             label="Email"
-            variant="underlined"
             placeholder="Digite seu email"
-            onClear={() => console.log("input cleared")}
+            variant="underlined"
+            {...register("email")}
+            isInvalid={!!errors.email}
             size="lg"
           />
+          {errors.email && (
+            <span className="text-red-500 text-sm mt-1 ml-2">
+              {errors.email.message}
+            </span>
+          )}
         </div>
         <div className="w-full">
           <Input
-            label="Password"
-            variant="underlined"
+            label="Senha"
             placeholder="Digite sua senha"
-            size="lg"
-            className="w-full"
+            variant="underlined"
+            {...register("password")}
             endContent={
               <button
+                aria-label="toggle password visibility"
                 className="focus:outline-none"
                 type="button"
                 onClick={toggleVisibility}
-                aria-label="toggle password visibility"
               >
                 {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
               </button>
             }
+            isInvalid={!!errors.password}
+            size="lg"
             type={isVisible ? "text" : "password"}
           />
+          {errors.password && (
+            <span className="text-red-500 text-sm mt-1 ml-2">
+              {errors.password.message}
+            </span>
+          )}
         </div>
+        {serverError && (
+          <span className="text-red-500 text-sm mt-1">{serverError}</span>
+        )}
+        {successMessage && (
+          <span className="text-green-500 text-sm mt-1">{successMessage}</span>
+        )}
         <div className="w-full">
-          <Button color="primary" size="md" className="w-full">
-            Entrar
+          <Button
+            className="w-full flex items-center justify-center" // Alinha o conteúdo do botão
+            color="primary"
+            disabled={loading}
+            size="md"
+            type="submit"
+          >
+            {loading ? "Entrando" : "Entrar"}
           </Button>
         </div>
         <div className="w-full text-center">
           Ainda não possui uma conta? Realize o{" "}
           <NextLink
+            className="data-[active=true]:text-primary data-[active=true]:font-large text-primary"
             href={"/register"}
-            className={
-              "data-[active=true]:text-primary data-[active=true]:font-large text-primary"
-            }
           >
             Cadastro
           </NextLink>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,84 +1,128 @@
 "use client";
-import { title } from "@/components/primitives";
 import { Checkbox, Input, Button } from "@nextui-org/react";
 import { EyeSlashFilledIcon, EyeFilledIcon } from "@/components/icons";
 import { useState } from "react";
 import NextLink from "next/link";
-import { link as linkStyles } from "@nextui-org/theme";
+import { useForm, SubmitHandler, UseFormRegister } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(1, "Nome é obrigatório"),
+  email: z.string().email("Formato de email inválido"),
+  password: z.string().min(4, "A senha deve ter pelo menos 4 caracteres"),
+  agreeToTerms: z.boolean().refine((val) => val === true, {
+    message: "Você deve concordar com os termos e a política de privacidade",
+  }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const simulateApiCall = (data: FormData): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      data.email === "teste@teste.com"
+        ? resolve()
+        : reject("Email já cadastrado");
+    }, 1000);
+  });
+};
+
 export default function RegisterPage() {
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-  const toggleVisibility = () => setIsVisible(!isVisible);
+  const toggleVisibility = () => setIsVisible((prev) => !prev);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setLoading(true);
+    setServerError(null);
+    setSuccessMessage(null);
+
+    try {
+      await simulateApiCall(data);
+      setSuccessMessage(
+        "Registro bem-sucedido! Você sera redirecionado para a sua pagina."
+      );
+    } catch (error) {
+      setServerError(error as string);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <form className="container  w-full h-full flex flex-col gap-5 justify-between ">
-      <div className="flex flex-col text-left justify-center gap-2 ">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="container w-full h-full flex flex-col gap-5 justify-between"
+    >
+      <div className="flex flex-col text-left justify-center gap-2">
         <h1 className="text-2xl">Criar uma conta</h1>
         <h2 className="text-base">
           Cadastre-se para uma nova conta para começar
         </h2>
       </div>
-      <div className="flex flex-col justify-center h-full  w-full gap-4">
-        <div>
-          <Input
-            isClearable
-            type="name"
-            label="Nome"
-            variant="underlined"
-            placeholder="Digite seu nome"
-            onClear={() => console.log("input cleared")}
-            className="w-full"
-            size="lg"
-          />
-        </div>
-        <div>
-          <Input
-            isClearable
-            type="email"
-            label="Email"
-            variant="underlined"
-            placeholder="Digite seu email"
-            onClear={() => console.log("input cleared")}
-            size="lg"
-          />
-        </div>
-        <div className="w-full">
-          <Input
-            label="Password"
-            variant="underlined"
-            placeholder="Digite sua senha"
-            size="lg"
-            className="w-full"
-            endContent={
-              <button
-                className="focus:outline-none"
-                type="button"
-                onClick={toggleVisibility}
-                aria-label="toggle password visibility"
-              >
-                {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
-              </button>
-            }
-            type={isVisible ? "text" : "password"}
-          />
-        </div>
-        <div className="">
-          <Checkbox size="sm">
-            Eu concordo com os termos e a politica de privacidade
-          </Checkbox>
-        </div>
-        <div className="w-full">
-          <Button color="primary" size="md" className="w-full">
-            Entrar
-          </Button>
-        </div>
-        <div className="w-full text-center ">
+      <div className="flex flex-col justify-center h-full w-full gap-4">
+        <InputField
+          label="Nome"
+          type="text"
+          register={register("name")}
+          error={errors.name}
+        />
+        <InputField
+          label="Email"
+          type="email"
+          register={register("email")}
+          error={errors.email}
+        />
+        <InputField
+          label="Senha"
+          type={isVisible ? "text" : "password"}
+          register={register("password")}
+          error={errors.password}
+          endContent={
+            <button
+              className="focus:outline-none"
+              type="button"
+              onClick={toggleVisibility}
+              aria-label="toggle password visibility"
+            >
+              {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
+            </button>
+          }
+        />
+        <CheckboxField
+          register={register("agreeToTerms")}
+          error={errors.agreeToTerms}
+        />
+        {serverError && (
+          <span className="text-red-500 text-sm mt-1">{serverError}</span>
+        )}
+        {successMessage && (
+          <span className="text-green-500 text-sm mt-1">{successMessage}</span>
+        )}
+        <Button
+          type="submit"
+          color="primary"
+          size="md"
+          className="w-full"
+          disabled={loading}
+        >
+          {loading ? "Registrando..." : "Criar Conta"}
+        </Button>
+        <div className="w-full text-center">
           Já possui uma conta?{" "}
-          <NextLink
-            href={"/login"}
-            className={
-              "data-[active=true]:text-primary data-[active=true]:font-large text-primary"
-            }
-          >
+          <NextLink href="/login" className="text-primary font-bold">
             Entrar
           </NextLink>
         </div>
@@ -86,3 +130,51 @@ export default function RegisterPage() {
     </form>
   );
 }
+
+interface InputFieldProps {
+  label: string;
+  type: string;
+  register: ReturnType<UseFormRegister<FormData>>;
+  error?: { message?: string };
+  endContent?: React.ReactNode;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  type,
+  register,
+  error,
+  endContent,
+}) => (
+  <div className="w-full text-left">
+    <Input
+      label={label}
+      variant="underlined"
+      placeholder={`Digite seu ${label.toLowerCase()}`}
+      type={type}
+      {...register}
+      isInvalid={!!error}
+      size="lg"
+      endContent={endContent}
+    />
+    {error && (
+      <span className="text-red-500 text-sm mt-1 ml-2">{error.message}</span>
+    )}
+  </div>
+);
+
+interface CheckboxFieldProps {
+  register: ReturnType<UseFormRegister<FormData>>;
+  error?: { message?: string };
+}
+
+const CheckboxField: React.FC<CheckboxFieldProps> = ({ register, error }) => (
+  <div className="flex items-center flex-col">
+    <Checkbox {...register} size="sm">
+      Eu concordo com os termos e a política de privacidade
+    </Checkbox>
+    {error && (
+      <span className="text-red-500 text-sm mt-1 ml-2">{error.message}</span>
+    )}
+  </div>
+);
